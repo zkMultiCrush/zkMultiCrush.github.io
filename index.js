7a6c5a00767b09c97afa63ab55da4f7e165cce7c
zkMultiCrush <106901968+zkMultiCrush@users.noreply.github.com>
2022-06-07 01:44:03 +0530

    minified js

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
-const BASE_URL = "/crushIndex.html"
-const MAX_CRUSHES = 10
-
-const createNewElement = () => {
-    let crushCount = document.getElementById("mehboobas").childElementCount;
-
-    if (crushCount >= MAX_CRUSHES) {
-        // TODO: HTML TO BE ADDED HERE
-        alert("No more crushes allowed!");
-        return;
-    }
-
-    var newCrush = document.createElement('div');
-    newCrush.setAttribute('class', 'row mehbooba-row')
-    newCrush.innerHTML = `<input type="text" class="form-control mehbooba" name="mehbooba" placeholder="Kim Kardashian" aria-describedby="inputGroupPrepend" required>
+const BASE_URL="/crushIndex.html",MAX_CRUSHES=10,createNewElement=()=>{if(document.getElementById("mehboobas").childElementCount>=10){alert("No more crushes allowed!");return}var a=document.createElement("div");a.setAttribute("class","row mehbooba-row"),a.innerHTML=`<input type="text" class="form-control mehbooba" name="mehbooba" placeholder="Kim Kardashian" aria-describedby="inputGroupPrepend" required>
                             <div class="invalid-feedback">
                                 Please enter crush's name!
-                            </div>`
-    document.getElementById("mehboobas").appendChild(newCrush);
-}
-
-const fetchUserName = () => document.getElementById("aashiq").value.trim()
-
-const fetchCrushNames = () => [...document.getElementsByClassName("mehbooba")].map(cn => cn.value.trim())
-
-const encryptWithAES = (text) => {
-    return CryptoJS.AES.encrypt(text, "123").toString();
-};
-
-const generateURL = async () => {
-    const aashiq = fetchUserName()
-    const mehboobas = fetchCrushNames()
-
-    const message = await openpgp.createMessage({ text: aashiq });
-    const encrypted = await openpgp.encrypt({
-        message,
-        passwords: mehboobas,
-        config: { preferredCompressionAlgorithm: openpgp.enums.compression.zlib }
-    });
-
-    const encryptedBody = encodeURIComponent(encryptWithAES(encrypted));
-    const url = "https://" + window.location.hostname + BASE_URL + `?hash=${encryptedBody}` + `&name=${encodeURIComponent(aashiq)}`;
-    document.getElementById("finalURL").innerHTML = `<a href=${url} id="disclaimer-links">${url}</a>`;
-}
+                            </div>`,document.getElementById("mehboobas").appendChild(a)},fetchUserName=()=>document.getElementById("aashiq").value.trim(),fetchCrushNames=()=>[...document.getElementsByClassName("mehbooba")].map(a=>a.value.trim()),encryptWithAES=a=>CryptoJS.AES.encrypt(a,"123").toString(),generateURL=async()=>{let a=fetchUserName(),c=fetchCrushNames(),d=await openpgp.createMessage({text:a}),e=await openpgp.encrypt({message:d,passwords:c,config:{preferredCompressionAlgorithm:openpgp.enums.compression.zlib}}),f=encodeURIComponent(encryptWithAES(e)),b="https://"+window.location.hostname+"/crushIndex.html"+`?hash=${f}`+`&name=${encodeURIComponent(a)}`;document.getElementById("finalURL").innerHTML=`<a href=${b} id="disclaimer-links">${b}</a>`}
